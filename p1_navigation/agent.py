4bf7dee89005791a1e1a7f0fd4ad8ad1c843582e
Ram Prakash H <ramprakash@rampi.in>
2018-11-14 07:29:14 +0530

    Navigation code updated

--- p1_navigation/agent.py
+++ p1_navigation/agent.py
@@ -85,17 +85,20 @@ class Agent():
         """
         states, actions, rewards, next_states, dones = experiences
 
-        ## TODO: compute and minimize the loss
-        "*** YOUR CODE HERE ***"
-        Q_targets = rewards + gamma* self.qnetwork_target(next_states).detach().max(dim=1)[0].unsqueeze(1)*(1-dones)
-        Q_current = self.qnetwork_local(states).gather(1,actions)
-        
-        loss = F.mse_loss(Q_current,Q_targets)
-        
-        self.optimizer.zero_grad() #Zero the gradients
-        loss.backward() #Compute gradients
-        self.optimizer.step() #Update gradients
-        
+        # Get max predicted Q values (for next states) from target model
+        Q_targets_next = self.qnetwork_target(next_states).detach().max(1)[0].unsqueeze(1)
+        # Compute Q targets for current states 
+        Q_targets = rewards + (gamma * Q_targets_next * (1 - dones))
+
+        # Get expected Q values from local model
+        Q_expected = self.qnetwork_local(states).gather(1, actions)
+
+        # Compute loss
+        loss = F.mse_loss(Q_expected, Q_targets)
+        # Minimize the loss
+        self.optimizer.zero_grad()
+        loss.backward()
+        self.optimizer.step()
 
         # ------------------- update target network ------------------- #
         self.soft_update(self.qnetwork_local, self.qnetwork_target, TAU)                     
